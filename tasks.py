1a5f8e4bbc7b1324dc06e8113c1c16ab730d6662
Andrei K <1____1@bk.ru>
2019-11-06 22:24:38 +0300

    fix celery app

--- tasks.py
+++ tasks.py
@@ -4,7 +4,7 @@ from celery import Celery
 from celery.schedules import crontab
 
 from app import create_app
-from app.posts.parsers import vk_pars
+from app.posts.parsers import vk_parser
 
 load_dotenv()
 login = os.environ.get('vk_login')
@@ -15,9 +15,10 @@ celery_app = Celery('tasks', broker='redis://localhost:6379/0')
 
 @celery_app.task
 def get_vk_pics():
+    print('run pic parses')
     with flask_app.app_context():
-        vk_pars.write_posts(login,password, 30, -100817956, 'vk_pars1')
+        vk_parser.write_posts(login,password, 30, -100817956, 'vk_pars1')
 
 @celery_app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(crontab(minutes='*/10'), get_vk_pics.s())
+    sender.add_periodic_task(crontab(hour='*/5'), get_vk_pics.s())
