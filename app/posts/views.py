1ba8007bf86bce9c155c781847c3c605aaad4f07
Andrei K <1____1@bk.ru>
2019-11-18 00:02:55 +0300

    refactor posts views wrt flake8

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,15 +1,17 @@
-from flask import abort, render_template, Blueprint, current_app, flash, redirect, request, url_for
+from flask import abort, Blueprint, \
+                  flash, redirect, \
+                  render_template, request, url_for
 from flask_login import login_required, current_user
 from flask_paginate import Pagination, get_page_parameter
 
 from app.posts.models import Post, Comment
 from app.posts.forms import CommentForm, DeleteForm, PostAddForm
 from app.user.models import User
-from app.user.decorators import admin_required
 from app.utils import get_redirect_target
 
 
 blueprint = Blueprint("posts", __name__)
+
 
 @blueprint.route('/')
 def index():
@@ -19,21 +21,27 @@ def index():
     if q:
         search = True
     page = request.args.get(get_page_parameter(), type=int, default=1)
-    posts_list = Post.objects.order_by('-posted').paginate(page=page,per_page=10)
+    posts_list = Post.objects.order_by('-posted').paginate(page=page,
+                                                           per_page=10)
     pagination = Pagination(page=page,
-            total = Post.objects.count(),css_framework='bootstrap4',
-            search=search, record_name='posts')
-    return render_template('posts/index.html',page_title=title,
-            posts_list=posts_list, pagination=pagination)
+                            total=Post.objects.count(),
+                            css_framework='bootstrap4',
+                            search=search,
+                            record_name='posts')
+    return render_template('posts/index.html', page_title=title,
+                           posts_list=posts_list,
+                           pagination=pagination)
+
 
 @blueprint.route('/posts/<string:post_id>')
 def single_post(post_id):
-    my_post =  Post.objects(id=post_id).get()
+    my_post = Post.objects(id=post_id).get()
     if not my_post:
         abort(404)
     form = CommentForm(post_id=post_id)
-    return render_template('posts/single_post.html',page_title=my_post.title,
-            post=my_post,comment_form=form)
+    return render_template('posts/single_post.html', page_title=my_post.title,
+                           post=my_post, comment_form=form)
+
 
 @blueprint.route('/posts/<string:post_id>/edit')
 @login_required
@@ -46,8 +54,9 @@ def edit_post(post_id):
     form.tag.data = post.tag
     form.text.data = post.text
     return render_template("posts/edit_post.html",
-            page_title=page_title,
-            form=form,post_id=post_id)
+                           page_title=page_title,
+                           form=form, post_id=post_id)
+
 
 @blueprint.route('/posts/<string:post_id>/edit_post_proc', methods=['POST'])
 def edit_post_proc(post_id):
@@ -61,6 +70,7 @@ def edit_post_proc(post_id):
     flash('Edited post')
     return redirect(url_for("posts.index"))
 
+
 @blueprint.route('/posts/tag/<string:tag>')
 def view_tag(tag):
     title = f"Tag: {tag}"
@@ -69,25 +79,29 @@ def view_tag(tag):
     if q:
         search = True
     page = request.args.get(get_page_parameter(), type=int, default=1)
-    posts_list = Post.objects(tag=tag).paginate(page=page,per_page=10)
+    posts_list = Post.objects(tag=tag).paginate(page=page, per_page=10)
     pagination = Pagination(page=page,
-            total = Post.objects(tag=tag).count(), css_framework='bootstrap4',
-            search=search, record_name='posts')
-    return render_template('posts/tag_page.html',page_title=title,
-            posts_list=posts_list, pagination=pagination)
+                            total=Post.objects(tag=tag).count(),
+                            css_framework='bootstrap4',
+                            search=search, record_name='posts')
+    return render_template('posts/tag_page.html', page_title=title,
+                           posts_list=posts_list,
+                           pagination=pagination)
+
 
 @blueprint.route('/posts/comment', methods=['POST'])
 @login_required
 def add_comment():
     form = CommentForm()
     if form.validate_on_submit():
-        post=Post.objects(id=form.post_id.data).get()
-        comment=Comment(text=form.comment_text.data,
-                from_user=User.objects(id=current_user.id).get(),
-                post=post)
+        post = Post.objects(id=form.post_id.data).get()
+        comment = Comment(text=form.comment_text.data,
+                          from_user=User.objects(id=current_user.id).get(),
+                          post=post)
         comment.save()
         Post.objects(id=form.post_id.data).update_one(push__comments=comment)
     return redirect(get_redirect_target())
+
 
 @blueprint.route('/posts/delete_comment', methods=['POST'])
 @login_required
@@ -97,6 +111,7 @@ def delete_comment():
     comment = Comment.objects(id=id_).get()
     comment.delete()
     return redirect(get_redirect_target())
+
 
 @blueprint.route('/posts/delete_post', methods=['POST'])
 @login_required
@@ -114,19 +129,19 @@ def add_post():
     form = PostAddForm()
     title = "Add post"
     return render_template("posts/add_post.html", page_title=title,
-            form=form)
+                           form=form)
 
-@blueprint.route('/posts/add_post_proc',methods=['POST'])
+
+@blueprint.route('/posts/add_post_proc', methods=['POST'])
 def add_post_proc():
     form = PostAddForm()
-    #if form.validate_on_submit():
     user = User.objects(id=current_user.id).get()
     post = Post(title=form.title.data,
-            tag=form.tag.data,
-            text=form.text.data,
-            urls=form.url.data.split(),
-            user=user,
-            )
+                tag=form.tag.data,
+                text=form.text.data,
+                urls=form.url.data.split(),
+                user=user,
+                )
     post.save()
     flash('Add post')
-    return redirect(url_for("posts.index"))#redirect(get_redirect_target())
+    return redirect(url_for("posts.index"))
