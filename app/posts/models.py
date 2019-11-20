ba2c1cc300d3389d2391e0b537147932ad90816a
Andrei K <1____1@bk.ru>
2019-11-20 13:25:02 +0300

    flake8 check

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,5 +1,5 @@
 import datetime
-import mongoengine
+from mongoengine import CASCADE, PULL
 from app.db import db
 from app.user.models import User
 
@@ -7,11 +7,12 @@ from app.user.models import User
 class Comment(db.Document):
     text = db.StringField(required=True)
     created = db.DateTimeField(default=datetime.datetime.now())
-    from_user = db.ReferenceField(User,required=True)
-    post = db.ReferenceField('Post')#, reverse_delete_rule=mongoengine.CASCADE)
+    from_user = db.ReferenceField(User, required=True)
+    post = db.ReferenceField('Post')
 
     def __repr__(self):
         return f"Comment {self.text} by {self.from_user}"
+
 
 class Post(db.Document):
     title = db.StringField(required=True)
@@ -19,12 +20,12 @@ class Post(db.Document):
     text = db.StringField()
     urls = db.ListField(db.StringField())
     posted = db.DateTimeField(default=datetime.datetime.now)
-    comments = db.ListField(db.ReferenceField(Comment,reverse_delete_rule=mongoengine.PULL))
+    comments = db.ListField(db.ReferenceField(Comment,
+                                              reverse_delete_rule=PULL))
     user = db.ReferenceField(User)
+
     def __repr__(self):
         return f"Post {self.title}"
 
 
-Post.register_delete_rule(Comment,'post', mongoengine.CASCADE)
-
-
+Post.register_delete_rule(Comment, 'post', CASCADE)
