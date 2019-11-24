103d878dd5b2cfb6dc843f594143db72e676c1fa
Andrei K <1____1@bk.ru>
2019-11-24 23:19:24 +0300

    flake8

--- app/admin/views.py
+++ app/admin/views.py
@@ -3,7 +3,8 @@ from flask import Blueprint, render_template
 from app.user.decorators import admin_required
 from app.posts.models import Comment, Post
 
-blueprint = Blueprint('admin',__name__, url_prefix = '/admin')
+blueprint = Blueprint('admin', __name__, url_prefix='/admin')
+
 
 @blueprint.route('/')
 @admin_required
@@ -11,5 +12,6 @@ def admin_index():
     title = "ADMIN"
     posts_list = Post.objects.order_by('-posted').limit(5)
     comments_list = Comment.objects.order_by('-created').limit(10)
-    return render_template('admin/index.html', page_title=title, 
-            posts_list=posts_list, comments_list=comments_list)
+    return render_template('admin/index.html', page_title=title,
+                           posts_list=posts_list,
+                           comments_list=comments_list)
